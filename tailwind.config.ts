ee4e548744c2c33b930b125ba8a951791b9c9efb
Mickey <1306shivam@gmail.com>
2024-11-18 18:44:33 +0530

    footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/*/.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/*/.{js,ts,jsx,tsx,mdx}",
+    "./src/components/*/.{js,ts,jsx,tsx,mdx}",
+    "./src/app/*/.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
@@ -21,9 +21,26 @@ const config: Config = {
           lg: "80px",
         },
       },
+      fontFamily: {
+        custom: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+      },
+      fontSize: {
+        footer: "1.6rem", // Custom font size for the footer
+      },
     },
   },
   plugins: [],
 };
 
-export default config;
+export default config
